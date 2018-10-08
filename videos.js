c43ac57582fbe9bd58a0ee7c52e87d8b05be794f
Алешенька:3 <alexnagorny.an@gmail.com>
2018-10-08 15:17:22 +0300

    fix videos

--- videos.js
+++ videos.js
@@ -2,70 +2,70 @@ var utf8 = require('utf8');
 var request = require('request');
 
 module.exports = {
-    video1_1(id, callback){
+    video1_1(id, callback) {
         var link = `http://видео.атм-мотивация.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video1_1&utm_term=video+lesson+1&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
-    video1_2(id, callback){
+    video1_2(id, callback) {
         var link = `http://видео.атм-мотивация.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video1_2&utm_term=video+lesson+2&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
-    video1_3(id, callback){
+    video1_3(id, callback) {
         var link = `http://видео.атм-мотивация.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video1_3&utm_term=video+lesson+3&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
-    video4_1(id, callback){
+    video4_1(id, callback) {
         var link = `http://видео.атм-статистики.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video4_1&utm_term=video+lesson+1&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
-    video4_2(id, callback){
+    video4_2(id, callback) {
         var link = `http://видео.атм-статистики.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video4_2&utm_term=video+lesson+2&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
-    video4_3(id, callback){
+    video4_3(id, callback) {
         var link = `http://видео.атм-статистики.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video4_3&utm_term=video+lesson+3&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
-    video3_1(id, callback){
+    video3_1(id, callback) {
         var link = `http://видео.атм-фонды.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video3_1&utm_term=video+lesson+1&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
-    video3_2(id, callback){
+    video3_2(id, callback) {
         var link = `http://видео.атм-фонды.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video3_2&utm_term=video+lesson+2&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
-    video2_1(id, callback){
+    video2_1(id, callback) {
         var link = `http://видео.атм-фонды.рф/?utm_source=vk&utm_medium=message&utm_campaign=funnel_1&utm_content=video2_1&utm_term=video+lesson+1&id=${id}`;
         request('https://clck.ru/--?url=' + utf8.encode(link), function (error, response, body) {
             callback((error !== null) ? link : body)
         })
     },
 
-    video5_about:"https://clck.ru/EV7VK",
-    video5:"https://clck.ru/EV7VL",
-    video5_1_about:"https://clck.ru/EV7VM",
-    video6_about:"https://clck.ru/EV7VK",
-    video6:"https://clck.ru/EV7VL",
+    video5_about: "https://clck.ru/EQDa5",
+    video5: "https://clck.ru/EQDcV",
+    video5_1_about: "https://clck.ru/EQDcj",
+    video6_about: "https://clck.ru/EQDdC",
+    video6: "https://clck.ru/EQDeD",
 
-    video6_1_about:"https://clck.ru/EV7YX",
-    video6_2_about:"https://clck.ru/EV7VM",
-    video7_about:"https://clck.ru/EV7VM",
+    video6_1_about: "https://clck.ru/EQDeq",
+    video6_2_about: "https://clck.ru/EQDfN",
+    video7_about: "https://clck.ru/EQDfn",
 
 
     video5_pay(id, callback) {
